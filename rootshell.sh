6712ce7ef1df6302d9de1e7778f6f81c0d2d638b
⎝⎝ ᗯ I ᑭ Ξ Λ ᑌ T ⎠⎠ <wipeautcrafter@gmail.com>
2019-02-17 19:09:39 +0100

    Update rootshell.sh

--- rootshell.sh
+++ rootshell.sh
@@ -18,7 +18,7 @@ dtool 6 1 0 100
 
 # create netcat session
 (/usr/bin/nc -l -p 9999 -e /bin/sh -i
-# change led to green on connect
+# BROKEN: change led to green on connect
 dtool 6 1 0 0
 # create listening netcat session
 /usr/bin/nc -ll -p 9999 -e /bin/sh -i) &
