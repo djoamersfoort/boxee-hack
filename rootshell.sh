aac40858382c9039d6fc0ebad0b786030f80cea7
⎝⎝ ᗯ I ᑭ Ξ Λ ᑌ T ⎠⎠ <wipeautcrafter@gmail.com>
2019-02-17 19:10:27 +0100

    📦

--- rootshell.sh
+++ rootshell.sh
@@ -12,17 +12,17 @@
 #      #         netcat root shell         #
 #      # # # # # # # # # # # # # # # # # # #
 
-# change the led to red
+# change the logo to red
 dtool 6 2 0 0
 dtool 6 1 0 100
 
 # create netcat session
-(/usr/bin/nc -l -p 9999 -e /bin/sh -i
-# BROKEN: change led to green on connect
+(/usr/bin/nc -l -p 9999 -e /bin/sh
+# BROKEN: change logo to green on connect
 dtool 6 1 0 0
 # create listening netcat session
 /usr/bin/nc -ll -p 9999 -e /bin/sh -i) &
 
-# change the led to orange
+# change the logo to orange
 dtool 6 2 0 100
 dtool 6 1 0 100
