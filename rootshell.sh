7b7dd635af5a8feb2914ca1b6d5ade3e4d4651e9
⎝⎝ ᗯ I ᑭ Ξ Λ ᑌ T ⎠⎠ <wipeautcrafter@gmail.com>
2019-02-17 16:44:22 +0100

    📦 Boxee Root Shell

--- rootshell.sh
+++ rootshell.sh
@@ -5,16 +5,17 @@
 # by wipeautcrafter #
 # # # # # # # # # # #
 
-# fade the led to red
+# change the led to red
 dtool 6 2 0 0
 dtool 6 1 0 100
 
-# create new nc session
-killall nc
-/usr/bin/nc -ll -p 9999 -e /bin/sh &
-
-sleep 2
-
-# fade the led back to green
-dtool 6 2 0 100
+# create netcat session
+(/usr/bin/nc -l -p 9999 -e /bin/sh -i
+# change led to green on connect
 dtool 6 1 0 0
+# create listening netcat session
+/usr/bin/nc -ll -p 9999 -e /bin/sh -i) &
+
+# change the led to orange
+dtool 6 2 0 100
+dtool 6 1 0 100
