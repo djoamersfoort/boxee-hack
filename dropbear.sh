032aec88ab531d0035910f3543bb163dc3c3987c
⎝⎝ ᗯ I ᑭ Ξ Λ ᑌ T ⎠⎠ <wipeautcrafter@gmail.com>
2019-03-16 11:07:16 +0100

    Create dropbear.sh

---
+++ dropbear.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#  _                          _                _     
+# | |                        | |              | |    
+# | |__   _____  _____  ___  | |__   __ _  ___| | __ 
+# | '_ \ / _ \ \/ / _ \/ _ \ | '_ \ / _` |/ __| |/ / 
+# | |_) | (_) >  <  __/  __/ | | | | (_| | (__|   <  
+# |_.__/ \___/_/\_\___|\___| |_| |_|\__,_|\___|_|\_\ 
+#
+#      # # # # # # # # # # # # # # # # # # #
+#      # B Y   W I P E A U T C R A F T E R #
+#      #         dropbear root ssh         #
+#      # # # # # # # # # # # # # # # # # # #
+
+# change the logo to red
+dtool 6 2 0 0
+dtool 6 1 0 100
+
+# copy boxee to tmp
+cp /media/BOXEE/dropbear /tmp
+chmod +x /tmp/dropbear
+./tmp/dropbear -R -F -E -B -p 22
+
+# change the logo to orange
+dtool 6 2 0 100
+dtool 6 1 0 100
